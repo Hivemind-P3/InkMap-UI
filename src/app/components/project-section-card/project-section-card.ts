f3a29bb38e77eb070a951aff112e568de2b5e578
Axel <ajimenezq1@ucenfotec.ac.cr>
2026-03-30 18:35:28 -0600

    Agregar project dashboard

---
+++ src/app/components/project-section-card/project-section-card.ts
@@ -0,0 +1,17 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-project-section-card',
+  imports: [NgClass, RouterLink],
+  templateUrl: './project-section-card.html',
+  styleUrl: './project-section-card.scss',
+})
+export class ProjectSectionCard {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() icon: string = '';
+  @Input() image: string = '';
+  @Input() route: string = '';
+}
