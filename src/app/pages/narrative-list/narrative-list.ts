69e7e02054761c4561c95f1e9f3a7706d28155b6
StarryFinita <starryfinita@outlook.com>
2026-04-17 21:20:00 -0600

    El editor funciona de verdad, al menos

--- src/app/pages/narrative-list/narrative-list.ts
+++ src/app/pages/narrative-list/narrative-list.ts
@@ -1,36 +1,38 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { NarrativeService, Narrative } from '../../services/narrative.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-narrative-list',
   templateUrl: './narrative-list.html',
-  styleUrls: ['./narrative-list.scss'],
+  styleUrls: ['./narrative-list.scss']
 })
 export class NarrativeListComponent implements OnInit {
   @Input() projectId!: number;
   @Output() selected = new EventEmitter<Narrative>();
 
   narratives: Narrative[] = [];
+  private sub!: Subscription;
 
   constructor(private service: NarrativeService) {}
 
   ngOnInit(): void {
-    this.load();
-  }
-
-  load() {
-    this.service.list(this.projectId).subscribe((res) => {
+    this.sub = this.service.narratives$.subscribe((res) => {
       this.narratives = res;
     });
+
+    this.service.list(this.projectId).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
   create() {
     const title = prompt('Chapter Title');
     if (!title) return;
 
-    this.service.create(this.projectId, title).subscribe(() => {
-      this.load();
-    });
+    this.service.create(this.projectId, title).subscribe();
   }
 
   select(c: Narrative) {
